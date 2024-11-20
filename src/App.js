6350092aa084db7baab898e32eada1343d3aca64
thesystemsmith <abdulrasheed.techy@gmail.com>
2024-11-21 02:16:16 +0530

    setup react app and define the app component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import axios from "axios";
+import './App.css'
 
-function App() {
+function App(){
+  const [markdown, setMarkdown] = useState('')
+  const [htmlPreview, setHtmlPreview] = useState('')
+
+  const handleInputChange = async (e) =>{
+    const input = e.target.value
+    setMarkdown(input)
+
+    try {
+      const response = await axios.post('http;//localhost:5001/convert', {markdown: input})
+      setHtmlPreview(response.data.html)
+    } catch (error) {
+      console.log('error converting markdown to html ', error)
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Markdown Editor</h1>
+      <div className="editor-container">
+        <textarea
+          className="editor"
+          value={markdown}
+          onChange={handleInputChange}
+          placeholder="Type Markdown here....."
+        />
+        <div
+          className="preview"
+          dangerouslySetInnerHTML={{__html: htmlPreview}}
+        />
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
